7cf71c765431f9b40baf29af7e8554bfe3420ee6
Elaine Yeung <yeung.egs@gmail.com>
2017-05-03 00:39:13 +0000

    Add Task 1 - working version to slice last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 if number > 5:
-    print('{} {:d} {} {}'.format('Last digit of', number, 'is', 'and is greater than 5'))
+    print('{} {:d} {} {:=1d} {}'.format('Last digit of', number, 'is', number, 'and is greater than 5'))
 elif number == 0:
-    print('{} {:d} {} {}'.format('Last digit of', number, 'is', 'and is 0'))
+    print('{} {:d} {:=5d} {} {}'.format('Last digit of', number, 'is', number, 'and is 0'))
 elif number < 6:
-    print('{} {:d} {} {}'.format('Last digit of', number, 'is', 'and is less than 6 and not 0'))
+    print('{} {:d} {:-1d} {} {}'.format('Last digit of', number, 'is', number, 'and is less than 6 and not 0'))
