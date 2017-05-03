5423c053ca2b500338020a8a2960b673d753fac3
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-02 23:35:15 -0700

    Add Task 8 - v1

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,6 @@ def uppercase(str):
     for c in str:
         if ord(c) >= ord('a') and\
            ord(c) <= ord('z'):
-            return True
-        return False
-    
-
-                
+            c = chr(ord(c) - ord(' '))
+        print('{:s}'.format(c), end='')
+    print()
