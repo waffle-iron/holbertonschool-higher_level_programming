752f5892d353114d2c6d8c78d70b7c532ae93c63
Elaine Yeung <yeung.egs@gmail.com>
2017-05-03 00:27:36 +0000

    Add Task 1 - fix if statement syntax

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,5 +5,5 @@ if number > 5:
     print('{} {:d} {} {}'.format('Last digit of', number, 'is', 'and is greater than 5'))
 elif number == 0:
     print('{} {:d} {} {}'.format('Last digit of', number, 'is', 'and is 0'))
-else number < 6:
+elif number < 6:
     print('{} {:d} {} {}'.format('Last digit of', number, 'is', 'and is less than 6 and not 0'))
