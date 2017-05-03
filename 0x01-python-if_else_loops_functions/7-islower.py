9ba2313f5bbabf3ec0ae191cd80ae15d9df92c51
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-02 23:26:28 -0700

    Add Task 7 - v1

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    for i in range(ord('a'), ord('z')):
-        if c == i:
+    for i in range(ord('a'), ord('z') + 1):
+        if ord(c) == i:
             return True
     return False
