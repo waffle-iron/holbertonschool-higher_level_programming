343121ba47035eee6fd3ebb065c4752bd92dcf3e
Elaine Yeung <yeung.egs@gmail.com>
2017-05-04 03:19:54 +0000

    Add Task 101 - post PLD version

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     list(str)
+    new_str = ''
     for i in range(len(str)):
         if i != n:
-            return new
+            new_str = str[i] + new_str
+    return new_str
