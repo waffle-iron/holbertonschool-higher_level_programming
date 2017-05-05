4be3087ce0e08a987f539f3b436344a9307b64bd
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-04 21:21:34 -0700

    Add Task 2 - adjust print format strings

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     import sys
     count = len(sys.argv)
     if count == 1:
-        print("0 argument.")
+        print("{}".format('0 argument.'))
     if count == 2:
-        print ("1 argument:")
+        print("{}".format('1 argument:'))
     if count > 2:
-        print("{:d}: arguments:".format(count - 1))
+        print("{:d} arguments:".format(count - 1))
     for i in range(1, count):
         print("{:d}: {:s}".format(i, sys.argv[i]))
