e3dced9e6f4441eb332d318b93673a6ed0915128
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-04 21:03:05 -0700

    Add Task 2 - adjusting arg count print statement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     if count > 2:
         print("{:d}: arguments:".format(count))
     for i in range(1, count):
-       print("{:d}: {:s}".format(i, sys.argv[i]))
+       print("{:d}: {:s}".format(i - 1, sys.argv[i]))
 
 
