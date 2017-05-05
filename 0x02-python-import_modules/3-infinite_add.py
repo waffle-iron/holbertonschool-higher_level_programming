06d1737857babc9bf507659daec2b48ad7ce7c1f
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-04 21:39:57 -0700

    Add Task 3 - fix PEP 8 formatting

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     count = len(sys.argv)
     sum = 0
-    for i in range (1, count):
+    for i in range(1, count):
         sum = int(sys.argv[i]) + sum
     print(sum)
