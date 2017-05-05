b7add3d452a1409bc5efe31c01c9440710bc0e9e
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-04 20:42:58 -0700

    Add Task 2 - working version
    Move if statements outside of for loop

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,11 @@
 if __name__ == "__main__":
     import sys
     count = len(sys.argv)
+    if count == 1:
+        print("0 argument.")
+    if count == 2:
+        print ("1 argument:")
+    if count > 2:
+        print("{:d} arguments:".format(count)
     for i in sys.argv[1:]:
-        if count > 1:
-            print("{:d}\n {}".format(count, sys.argv[1:]))
+              print("{:d}\n {}".format(count, sys.argv[1:]))
