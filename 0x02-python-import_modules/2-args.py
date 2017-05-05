6a62b8acf7bd75fc2cd785b7f0d895b3400ab675
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-04 20:35:32 -0700

    Add count variable for

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    count = len(sys.argv)
     for i in sys.argv[1:]:
-        if len(sys.argv) > 1:
-            print("{:d}\n {}".format(len(sys.argv), sys.argv[1:]))
+        if count > 1:
+            print("{:d}\n {}".format(count, sys.argv[1:]))
