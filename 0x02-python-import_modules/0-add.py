05ccabfd527f39e474ed51f2f6b4b671587ce28f
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-04 17:09:39 -0700

    Add Task 0 - v1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+from add_0 import add
+a = 1
+b = 2
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
