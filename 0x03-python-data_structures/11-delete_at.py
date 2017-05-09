b06ff0b7f23857cbbe7213502ee18ca7c127cf38
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-09 15:06:46 -0700

    Update Task 11 PEP 8 formatting

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list):  # check if index is in range
-        return (my_list)
+        return(my_list)
     if my_list is None:  # check if my_list exists
-        return (None)
+        return(None)
     del my_list[idx]
     return(my_list)
