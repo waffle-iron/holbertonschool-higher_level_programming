b5b191934d77314c7e73e8f67428320a92ffbb17
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-09 13:15:41 -0700

    Add Task 4 - adding .copy function

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -8,9 +8,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0:
-        return(my_list)
-    listcopy = my_list
-    listcopy[idx] = element
-    return(listcopy)
-
-    
+        return(my_list.copy())
+    my_list[idx] = element
+    return my_list.copy()
