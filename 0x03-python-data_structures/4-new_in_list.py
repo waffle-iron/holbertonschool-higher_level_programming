69235f3ac443b61f23cd0b56011c8ea01c4e016a
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-09 14:23:44 -0700

    Update Task 4 to check length of copied list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     listcopy = my_list[:]
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > len(listcopy):
         return(listcopy)
     listcopy[idx] = element
     return(listcopy)
