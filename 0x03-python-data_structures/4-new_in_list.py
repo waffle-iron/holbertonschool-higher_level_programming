741eebd679086fb8911489e00944f83800d4127f
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-09 11:36:11 -0700

    Add Task 4 - working version, no output yet

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,12 @@
+# Write a function that replaces an element in a list at specific position without modifying the original list.
+# Prototype: def new_in_list(my_list, idx, element):
+# If idx is out of range, the function should
+# return a copy of the original list
+# You are not allowed to import any module
+# You are not allowed to use try/except
+
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return(my_list)
+    my_list.copy()
