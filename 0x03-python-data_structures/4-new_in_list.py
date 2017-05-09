33444c0e85aa1ee1bbb62a73e27218c9775dc077
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-09 11:39:36 -0700

    Add Task 4 - printing output, still incorrect

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -9,4 +9,8 @@
 def new_in_list(my_list, idx, element):
     if idx < 0:
         return(my_list)
-    my_list.copy()
+    listcopy = my_list
+    listcopy[idx] = element
+    return(listcopy)
+
+    
