d445036cc6aa6b0eed150f4c2fe14b971ffa97f4
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-09 16:38:58 -0700

    Update task  statements

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     listcopy = my_list[:]
-    if idx < 0 or idx > len(listcopy):
+    if listcopy is None:
+        return
+    if idx < 0 or idx > len(listcopy) - 1:
         return(listcopy)
     listcopy[idx] = element
     return(listcopy)
