0fd7521af19f2d2600a4f461ca1f4624ceb46e78
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-09 13:40:51 -0700

    Add Task 5 - v1

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            no_more_c += char
+    no_more_c = ""  # declare empty variable to be filled with output
+    for char in my_string:  # traverses through each character in string
+        if char != 'c' and char != 'C':  # exclude upper/lower case C
+            no_more_c += char  # store remaining characters into variable
     return(no_more_c)
-
