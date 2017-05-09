b82dd0472932accdd413f240a0d79502adbf0658
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-09 02:17:51 -0700

    Add Task 1 - fixed typos

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,5 +3,5 @@ def element_at(my_list, idx):
     if idx < 0:
         return (None)  # check if idx is out of range
     if idx < len(my_list):
-        return (list(idx))
+        return (my_list[idx])
     return(None)
