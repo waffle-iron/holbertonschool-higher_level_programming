f1f61dbb671939198ead1d28068a5ffd2f12b19f
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-09 14:00:52 -0700

    Revert Task 4 to previous commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -8,6 +8,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0:
-        return(my_list.copy())
-    my_list[idx] = element
-    return my_list.copy()
+        return(my_list)
+    listcopy = my_list
+    listcopy[idx] = element
+    return(listcopy)
+
+    
