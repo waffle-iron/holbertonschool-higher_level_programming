28b481b9973766a2af3a45cffdb6851b7ece9a53
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-09 21:41:03 -0700

    Add Task 7 - output matches expected, added for loops to insert ints to lists

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,9 +8,16 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len_tuple_b > 2:
         len_tuple_b = 2
 
+    # create list, also accounts for missing integer
     list_a = [0, 0]
     list_b = [0, 0]
 
-    new_tuple = (list_a[0] + list_b[0],
+    # loop through tuple, insert ints into list
+    for i in range(len_tuple_a):
+        list_a[i] += tuple_a[i]
+    for i in range(len_tuple_b):
+        list_b[i] += tuple_b[i]
+
+    tuple_sum = (list_a[0] + list_b[0],
                  list_a[1] + list_b[1])
-    return (new_tuple)
+    return (tuple_sum)
