98500222dc1e64f52a84e1a728ae86c2a19f9a4e
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-09 21:24:55 -0700

    Add Task 7 - working version - .append won't work, changing approach

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    len_tuple_a = len(tuple_a)
+    len_tuple_b = len(tuple_b)
+
+    if len_tuple_a > 2:
+        len_tuple_a = 2
+    if len_tuple_b > 2:
+        len_tuple_b = 2
+
+    list_a = [0, 0]
+    list_b = [0, 0]
+
     
