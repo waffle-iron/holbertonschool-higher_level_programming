cfd0e6f22e5c2a990ff2547653b2bbe308cec247
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-09 17:39:57 -0700

    Update Task 9 - change formatting

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list is None:
-        return None
+        return(None)
     my_list.sort()
     my_maximum = my_list[-1]
     return(my_maximum)
