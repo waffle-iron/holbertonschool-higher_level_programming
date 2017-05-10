a1636dba5d4bb1ed5607df05fbb01964cd8fc6f0
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-09 17:16:29 -0700

    Add Task 10 - working in progress, still no output

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     quotient = []  # empty list to be filled by function
     for q in my_list[:]:
         if my_list[q] % 2 == 0:
-            TRUE
+            quotient.append(TRUE)
         else:
-            FALSE
+            quotient.append(FALSE)
     return (quotient)
