092a05e311b94b474ae53d9ab2087f6b4b97e658
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-09 17:17:37 -0700

    Task 10 - v1 - fixed case of condition checks

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     quotient = []  # empty list to be filled by function
     for q in my_list[:]:
         if my_list[q] % 2 == 0:
-            quotient.append(TRUE)
+            quotient.append(True)
         else:
-            quotient.append(FALSE)
+            quotient.append(False)
     return (quotient)
