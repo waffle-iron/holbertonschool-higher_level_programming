56e1e0b0e1a091f354fe10031b5035c9a6beed83
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-09 21:33:24 -0700

    Add Task 7 - working version, output is all zeroes, not expected

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -11,4 +11,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     list_a = [0, 0]
     list_b = [0, 0]
 
-    
+    new_tuple = (list_a[0] + list_b[0],
+                 list_a[1] + list_b[1])
+    return (new_tuple)
