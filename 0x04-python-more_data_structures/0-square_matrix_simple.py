4bd0b1d02c0043b366d24a4a6b724227f1e6ab2d
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-11 16:01:12 -0700

    Add Task 0 - add lambda function to square x

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,19 +2,8 @@
 def square_matrix_simple(matrix=[]):
     if matrix is None:  # check if matrix exists
         return None
-
-    if len(matrix[:]) == 0:  # check length of matrix
-        print()
+    if matrix == []:  # return if matrix is empty
         return
-
-    for row in range(0, len(matrix=[:])):
-        for idx in matrix[row]:
-            print("{:d}, ".format(squareit(int),end=""))
-            row_size = len(matrix[row]) -1
-            row_index = matrix[row].index(idx)
-            if row_index != row_size:  # check size of row_index
-                print(' ', end="")  # print spaces between numbers
-            else:
-                print()  # insert new line once index equals size of row
-
-
+    squareit = lambda x : x*x
+    matrix_squared = [[squareit(x) in row] for row in matrix]
+    return (squared_matrix)
