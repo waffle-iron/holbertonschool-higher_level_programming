74ee4f4d1ddd323bd72ab70337627169883982e4
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-11 15:11:19 -0700

    Update Task 0 - working version

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    if matrix is None:  # check if matrix exists
+        return None
+
+    if len(matrix[:]) == 0:  # check length of matrix
+        print()
+        return
+
+    for row in range(0, len(matrix=[:])):
+        for idx in matrix[row]:
+            print("{:d}, ".format(squareit(int),end=""))
+            row_size = len(matrix[row]) -1
+            row_index = matrix[row].index(idx)
+            if row_index != row_size:  # check size of row_index
+                print(' ', end="")  # print spaces between numbers
+            else:
+                print()  # insert new line once index equals size of row
+
+
