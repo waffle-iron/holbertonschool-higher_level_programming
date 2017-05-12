9dd73dad146e26694c60767398f19d1aefa71b80
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-11 18:02:37 -0700

    Update Task 1 - PEP 8 formatting

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    idx_to_replace = [i for i, x in enumerate(new_list) if x==search]
+    idx_to_replace = [i for i, x in enumerate(new_list) if x == search]
     for i in idx_to_replace:
         new_list[i] = replace
     return(new_list)
