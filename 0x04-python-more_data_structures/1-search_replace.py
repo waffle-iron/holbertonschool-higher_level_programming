1b678476082424570fc3a1e58ba379474406a5ea
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-11 17:07:05 -0700

    Add Task 1 - working version - no output

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    
+    for number in my_list:
+        if number == search:
+            number = replace
+        else:
+            number
+    return(my_list)
+
+return [replace if item == search else item for item in my_list]
