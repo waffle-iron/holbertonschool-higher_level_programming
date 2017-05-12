2d4daa622bda81d252f90602ef9513a4bbbbb76f
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-11 17:30:04 -0700

    Task 1 - still :-1:

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+
     for number in my_list:
         if number == search:
-            number = replace
+            replace
         else:
             number
     return(my_list)
