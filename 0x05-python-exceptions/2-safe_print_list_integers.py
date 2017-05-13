92c93931ef57adebaa8627d21893505a1f9a6486
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-12 19:17:15 -0700

    Update Task 2 to include print formatter {:d}

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for item in range(x):
         try:
-            print("{}".format(my_list[item]), end="")
+            print("{:d}".format(my_list[item]), end="")
             count += 1
         except (ValueError, TypeError):
             continue
