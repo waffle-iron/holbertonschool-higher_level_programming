b361ce153c79acad3bc83819b3e06d16aec4cc16
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-12 18:42:39 -0700

    Task 0 - move if statement inside try

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -13,11 +13,11 @@ def safe_print_list(my_list=[], x=0):
 
     count = 0
     for item in my_list:
-        if count < x:
-            try:
+        try:
+            if count < x:
                 print("{}".format(item), end="")
                 count += 1
-            except (IndexError, TypeError):
-                pass
+        except (IndexError, TypeError):
+            pass
     print("")
     return(count)
