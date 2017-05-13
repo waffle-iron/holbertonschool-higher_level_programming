8a2af1441bb8eba4ce9f3599719a7a7abd69ce30
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-13 03:54:39 -0700

    Set key to equal None

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(my_dict):
     if my_dict is None:
         return None
     max_score = 0
-    key = 0
+    key = None
     for k, v in my_dict.items():
         if v > max_score:
             max_score = v
