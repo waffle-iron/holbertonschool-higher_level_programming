a8f7303788bc120ffe5d5c417387c7faba873c5c
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-12 18:22:06 -0700

    Task 0 - move iterator inside try statement

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,9 +15,9 @@ def safe_print_list(my_list=[], x=0):
     if count < x:
         for item in my_list:
             try:
-                print("{}".format(my_list[count]),end="")
+                print("{}".format(item), end="")
+                count += 1
             except (IndexError, TypeError):
                 break
-            count += 1
     print("")
     return(count)
