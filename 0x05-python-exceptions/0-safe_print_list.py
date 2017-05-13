9644d799b89fd38439e66f123555331aeb6ddb1a
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-12 17:42:51 -0700

    Update Task 0 to remove line breaks

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -14,7 +14,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for i, count in enumerate(my_list):
         try:
-            print("{}".format(my_list[i]))
+            print("{}".format(my_list[i]),end="")
         except (IndexError, TypeError):
             print("{}".format("no can do!"))
     return(count)
