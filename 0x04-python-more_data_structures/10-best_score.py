053e3cce6b9409bffa939126bcc194ac3c29937f
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-13 03:51:13 -0700

    Rename variable

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 def best_score(my_dict):
+    """
+    Write a function that returns a key with the biggest integer value.
+    You can assume that all values are only integers
+    If no score found, return None
+    You can assume all students have a different score
+    You are not allowed to import any module
+    """
     if my_dict is None:
         return None
-    best_score = 0
+    max_score = 0
     key = 0
     for k, v in my_dict.items():
-        if v > best_score:
-            best_score = v
+        if v > max_score:
+            max_score = v
             key = k
     return key
