905c9061c3c425f2ba3bd98098f14be8dee444b0
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-12 17:59:06 -0700

    Task 0 - iterate count

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,8 +15,9 @@ def safe_print_list(my_list=[], x=0):
     if count < x:
         for x, count in enumerate(my_list):
             try:
-                print("{}".format(my_list[x]),end="")
+                print("{}".format(my_list[count]),end="")
             except (IndexError, TypeError):
                 break
+            count += 1
     print("")
     return(count)
