eb31b1ef62921ede176ab3f7ba697e2a9a43f9a5
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-12 19:08:42 -0700

    Task 2 - work in progress, not matching expected output at all

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for item in my_list:
+        try:
+            print("{}".format(item), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return(count)
