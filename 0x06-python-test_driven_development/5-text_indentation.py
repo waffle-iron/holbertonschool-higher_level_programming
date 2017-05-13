edc1424abc6d9e1225e5fcd920a3612d3a3e39db
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-13 13:08:30 -0700

    Update Task 5 with exception messages

--- 0x06-python-test_driven_development/5-text_indentation.py
+++ 0x06-python-test_driven_development/5-text_indentation.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
+"""
+This module is task 4 for 0x06-python-test_driven_development
+
+This module supplies one function: `text_indentation`
+
+To test, a text file is provided in the /tests directory. To run:
+`python3 -m doctest ./tests/5-text_indentation.txt`
+"""
 def text_indentation(text):
-    
-    
-    
-    
-    
+    """
+    Returns text from argument with 2 new lines after specific characters
+
+    Arguments:
+    text: must be a string
+    """
+    if len(text) < 0 or text is None or not isinstance(text, str):
+        raise TypeError('text must be a string')
