33e8af37c2d1da664d2cd076339a584eb7ef40c7
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-13 05:05:28 -0700

    :construction: Add Task 0 - work in progress

---
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_integer(a, b):
+    """Returns a + b.
+
+    Works with integers:
+
+    >>> add_integer(2, 3)
+    5
+    """
+    if isinstance(a, int) and isinstance(b, int):
+        return a + b
+    return a + b
