36781f171fa040df79ecbfd862312437b8dfbe32
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-13 03:46:00 -0700

    Update Task 10 - push to remote

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    
+    if my_dict is None:
+        return None
+    best_score = 0
+    key = 0
+    for k, v in my_dict.items():
+        if v > best_score:
+            best_score = v
+            key = k
+    return key
