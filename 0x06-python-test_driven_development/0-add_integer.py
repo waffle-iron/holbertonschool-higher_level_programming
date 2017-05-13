538cbbbc78dfa2e0bd7d1345313069accd06a6cf
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-13 07:57:54 -0700

    Cast a & b as int() and store into new variables

--- 0x06-python-test_driven_development/0-add_integer.py
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
+"""
+This module is task 0 for 0x06-python-test_driven_development
+
+This module supplies one function: add_integer
+
+To test, a text file is provided in the /tests directory. To run:
+`python3 -m doctest -v ./tests/0-add_integer.txt`
+"""
 def add_integer(a, b):
-    """Returns a + b.
+    """
+    This function calculates the sum of two integers
 
-    Works with integers:
+    Arguments:
+    a: integer, if type float, will be cast to integer
+    b: integer, if type float, will be cast to integer
 
-    >>> add_integer(2, 3)
-    5
+    Return:
+    a + b or raise error if input is not an integer
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    int(a, b)
-    return a + b
+    cast_a = int(a)
+    cast_b = int(b)
+    return (cast_a + cast_b)
