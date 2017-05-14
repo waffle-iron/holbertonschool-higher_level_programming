7fe8adb645f011bdc633fafcf26e5d323eb4762d
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-13 18:55:28 -0700

    Update with some unittests

--- 0x06-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x06-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,4 +5,11 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    
+    def test_list_with_only_ints(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+    def test_list_with_not_an_int(self):
+        with self.assertRaises(TypeError):
+            max_integer(['a', 2, 3, 4])
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
