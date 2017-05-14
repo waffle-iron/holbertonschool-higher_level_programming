5e7b710fa5b72ecb6ca331353ba188860957eca9
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-13 18:52:02 -0700

    Update files with correct names

--- 0x06-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x06-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
+"""Unittest for max_integer([..])
 """
+import unittest
+max_integer = __import__('6-max_integer').max_integer
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+class TestMaxInteger(unittest.TestCase):
+    
