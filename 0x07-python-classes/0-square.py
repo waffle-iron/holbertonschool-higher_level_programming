b76758a6ff911fe192e4eb1b7108c0fcd897f887
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-19 09:42:33 -0700

    Update Task 0 - v1

--- 0x07-python-classes/0-square.py
+++ 0x07-python-classes/0-square.py
@@ -1,1 +1,8 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""This module "0-square" is task 0 for 0x07-python-classes
+and completed for Holberton School coursework.
+"""
+class Square:
+    """This class defines a square
+    """
+    pass
