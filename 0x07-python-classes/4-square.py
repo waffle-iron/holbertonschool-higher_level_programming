a44ce10a05bb20b41cf104faea2af109fd247312
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-19 11:16:22 -0700

    Add Task 4 - :construction: work in progress, still returning errors

--- 0x07-python-classes/4-square.py
+++ 0x07-python-classes/4-square.py
@@ -1,1 +1,65 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""
+This module "4-square" is task 4 for 0x07-python-classes
+and completed for Holberton School coursework.
+
+This module contains one class: Square
+"""
+
+
+class Square:
+    """
+    This class defines a square based on 3-square
+
+    **Task parameters:**
+    Private instance attribute: size
+    Instantiation with optional size: def __init__(self, size=0)
+
+    Public instance method: def area(self): returns current square area
+    You are not allowed to import any module
+
+    **Functions**
+    __init__(self, size)
+    area(self)
+
+    **Example**
+    my_square_1 = Square(3)
+    """
+
+    def __init__(self, size=0):
+        """
+        instantiate an object based on Square class
+
+        Arguments:
+        size must be an integer
+        size must be >= 0
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        property getter def size(self) to retrieve it
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        property setter def size(self, value) to set size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        public instance to return square area
+
+        return:
+        area of a square(int)
+
+        """
+        return(self.__size * self.__size)
