39ffc3061b49036ee8a792005fdb7b46208af9df
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-19 15:11:07 -0700

    Add Task 0 - v1

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module "0-rectangle" is task 0 for 0x08-python-more_classes
+and completed for Holberton School coursework.
+"""
+
+
+class Rectangle:
+    """
+    This class defines a rectangle
+    """
+    pass
+
