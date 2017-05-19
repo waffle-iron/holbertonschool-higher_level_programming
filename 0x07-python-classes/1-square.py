ddc0b207f4dade604bcaf921da901645cb16cb02
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-19 10:04:03 -0700

    Add Task 1 - v1

--- 0x07-python-classes/1-square.py
+++ 0x07-python-classes/1-square.py
@@ -1,1 +1,33 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
+"""
+This module "1-square" is task 1 for 0x07-python-classes
+and completed for Holberton School coursework.
+
+This module contains one class: Square
+"""
+
+
+class Square:
+    """
+    This class defines a square based on 0-square
+
+    **Task parameters:**
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    You are not allowed to import any module
+
+    **Functions**
+    __init__(self, size)
+
+    **Example**
+    my_square = Square(3)
+    """
+
+    def __init__(self, size):
+        """
+        instantiate an object based on Square class
+
+        Arguments:
+        size - no type/value verification
+        """
+        self.__size = size
