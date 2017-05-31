25b56fc937ba161ca5b9472eb199bd671469f66b
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-31 06:03:49 -0700

    Add Task 4 - still in progress

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
-def append_write(filename="", text=""):
-    """Write a function that appends a string at the end of a text file (UTF8) and returns the number of characters added:
+"""
+This is module 4-append_write
 
-If the file doesn't exist, it should be created
-You must use the with statement
-You don't need to manage file permission/file doesn't exist exceptions.
-You are not allowed to import any module
+This module contains one function `append_write`
+"""
+
+
+def append_write(filename="", text=""):
     """
-    
+    Write a function that appends a string at the end of a text file (UTF8) and returns the number of characters added:
+    If the file doesn't exist, it should be created
+    You must use the with statement
+    You don't need to manage file permission/file doesn't exist exceptions.
+    You are not allowed to import any module
+
+    Args:
+    filename
+    text
+
+    Return:
+    number of characters added
+    """
+    with open(filename, "a", encoding='utf-8') as my_file:
+        return(my_file.write(my_file.write(text)))
