47cb916cda26657efccc4468daa4f396322187ea
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-31 07:04:57 -0700

    Add Task 6 - v1

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
-def from_json_string(my_str):
-    """Write a function that returns an object (Python data structure) represented by a JSON string:
-You don't need to manage exceptions if the JSON string doesn't represent an object.
+import json
 """
-    
+This is module 6-from_json_string
+
+This module contains one function `from_json_string`
+"""
+
+def from_json_string(my_str):
+    """
+    Write a function that returns an object (Python data structure) represented by a JSON string:
+    You don't need to manage exceptions if the JSON string doesn't represent an object.
+
+    Args:
+    my_str
+
+    Return:
+    object represented by a JSON string
+    """
+    return json.loads(my_str)
