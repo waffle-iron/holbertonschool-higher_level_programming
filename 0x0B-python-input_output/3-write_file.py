dc613f7347f29ef9c0ccdba2f8ef9004e905337a
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-31 05:07:14 -0700

    Update Task 3 - work in progress

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
-def write_file(filename="", text=""):
-    """Write a function that writes a string to a text file (UTF8) and returns the number of characters written:
+"""
+This is module 3-write_file
 
-You must use the with statement
-You don't need to manage file permission exceptions.
+This module contains one function `write_file`
+"""
+
+
+def write_file(filename="", text=""):
     """
-    
+    Write a function that writes a string to a text file (UTF8) and returns the number of characters written:
+    You must use the with statement
+    You don't need to manage file permission exceptions.
+
+    Args:
+    filename
+    text
+
+    Return:
+    number of characters written
+    """
+    with open(filename, "r", encoding='utf-8') as my_file:
+        print(my_file.tell())
