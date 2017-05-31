cad17bc3b12cd8ce6554c43b0db131edeffa12b4
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-30 18:55:52 -0700

    Add Task 6 - work in progress

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+This is module 6-base_geometry
+
+This module contains one class, based on 5-base-geometry
+"""
+
+
+class BaseGeometry:
+    """
+    This class contains one public instance method:
+    `def area(self)`
+
+    Raises:
+    Exception with the message
+    """
+
+    raise ("area() is not implemented")
