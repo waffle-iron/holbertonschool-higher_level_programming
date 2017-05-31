deaf0e9c2012443c3dbf9eacfb23ee5847f7ed0d
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-31 07:44:15 -0700

    Add Task 7 - v1

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-def load_from_json_file(filename):
-    """Write a function that creates an Object from a "JSON file":
+import json
+"""
+This is module 7-save_to_json_file
 
-You must use the with statement
-You don't need to manage exceptions if the JSON string doesn't represent an object.
-You don't need to manage file permissions / exceptions.
+This module contains one function `save_to_json_file`
+"""
+
+def save_to_json_file(my_obj, filename):
     """
-    
+    Write a function that writes an Object to a text file, using a JSON representation:
+    You must use the with statement
+    You don't need to manage exceptions if the object can't be serialized.
+    You don't need to manage file permission exceptions.
+
+    Args:
+    my_obj
+    filename
+    """
+    with open(filename, 'w') as my_file:
+        my_file.write(json.dumps(my_obj))
