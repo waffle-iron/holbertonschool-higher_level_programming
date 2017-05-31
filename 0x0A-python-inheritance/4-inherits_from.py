8b781059b445bdc8144b2948c720e9fdff7e8098
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-30 18:40:16 -0700

    Add Task 4 - work in progress :construction:

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+This is module 4-inherits_from
+
+This module contains one function
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    checks instance of a specified class
+
+    Args:
+    obj - object
+    a_class - specified class
+
+    Returns:
+    True if the object is an instance of a class
+    that inherited (directly or indirectly)
+    from the specified class ;
+    otherwise False.
+    """
+    return issubclass(a_class, a_class)
