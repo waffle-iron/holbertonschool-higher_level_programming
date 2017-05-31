15667ff32ddda623d77ab49ec9641176fab3000c
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-31 06:25:22 -0700

    Update Task 4 - v1 - changed return statement to match task 3

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -23,5 +23,5 @@ def append_write(filename="", text=""):
     """
     with open(filename, 'a', encoding='utf-8') as my_file:
         my_file.write(text)
-    with open(filename, 'r', encoding='utf-8') as my_file:
-        return(my_file.read(text))
+    with open(filename, 'r+', encoding='utf-8') as my_file:
+        return(my_file.write(text))
