f6b6e418d2152375c2aa79b97a984ac1ef8924cd
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-31 03:39:42 -0700

    Add Task 0 - working version

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+"""
+This is module 0-read_file
+
+This module contains one function `read_file()`
+"""
+
+
 def read_file(filename=""):
-    """a function that reads a text file (UTF8) and prints it to stdout
+    """
+    a function that reads a text file (UTF8) and prints it to stdout
     You must use the with statement
     You don't need to manage file permission/file doesn't exist exceptions.
     """
-    
+    with open(self, encoding='utf-8') as my_file:
+        print(my_file)
