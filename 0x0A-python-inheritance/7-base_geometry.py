362700d6b7b00992a9d068d53f76e919f45d2804
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-30 18:56:36 -0700

    Add Task 7 - work in progress

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+"""
+This is module 7-base_geometry
+
+This module contains:
+-one class
+-two functions
+
+Access test for this class in /tests subfolder - to run:
+`python 3 -m doctest ./tests/7-base_geometry.txt`
+"""
+
+
+class BaseGeometry:
+    """
+    base geometry class
+
+    Public instance method:
+    `def area(self)`
+    `def integer_validator(self, name, value)`
+
+    Args:
+    name
+    value
+    Returns:
+    area of shape
+    """
+
+    def area(self):
+        """
+        returns the area
+        """
+        
+    def integer_validator(self, name, value):
+         """
+         validates value
+         """
