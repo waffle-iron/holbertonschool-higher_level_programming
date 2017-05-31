1e7c696b4891a94d0413cdf3ede7cf99472a443d
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-31 06:54:09 -0700

    Add Task 5 - v1

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
-def to_json_string(my_obj):
-    """Write a function that returns the JSON representation of an object (string):
+import json
+"""
+This is module 5-to_json_string
 
-You don't need to manage exceptions if the object can't be serialized.
+This module contains one function `to_json_string`
+"""
+
+def to_json_string(my_obj):
     """
-    
+    Write a function that returns the JSON representation of an object (string):
+    You don't need to manage exceptions if the object can't be serialized.
+
+    Args:
+    my_obj
+
+    Return:
+    JSON representation of object
+    """
+    return json.dumps(my_obj)    
