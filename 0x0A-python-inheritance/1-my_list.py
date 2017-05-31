c5dd19e397017e30ce5ea298895eb3442ade879e
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-30 17:43:28 -0700

    Update PEP8 formatting

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,6 +3,9 @@
 This is module 1-my_list
 
 This module contains one class and one public instance method
+
+Access test for this class in /tests subfolder - to run:
+`python 3 -m doctest ./tests/1-my_list.txt`
 """
 
 
@@ -11,12 +14,13 @@ class MyList(list):
     inherits from `list`
 
     Public instance method:
-    `def print_sorted(self):` 
+    `def print_sorted(self):`
 
     Args:
 
     Returns:
     """
+
     def print_sorted(self):
         """
         prints the list, but sorted (ascending sort)
