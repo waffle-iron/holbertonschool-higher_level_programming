55c749f2772aff9fe14d42e846f2cd94b8473b92
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-30 17:33:24 -0700

    Task 1 - work in progress

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+This is module 1-my_list
+
+This module contains one class and one public instance method
+"""
+
+
+class MyList(list):
+    """
+    inherits from `list`
+
+    Public instance method:
+    `def print_sorted(self):` that prints the list, but sorted (ascending sort)
+
+    Args:
+
+    Returns:
+    """
+    def print_sorted(self):
+        list(self)
