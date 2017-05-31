3a9dffbe4fe2c7a5380509487807b0876b939b34
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-31 03:59:54 -0700

    Add Task 1 - v1

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python3
+"""
+This is module 0-read_file
+
+This module contains one function `read_file()`
+"""
+
+
+#!/usr/bin/python3
 def number_of_lines(filename=""):
     """Write a function that returns the number of lines of a text file
     You must use the with statement
     You don't need to manage file permission/file doesn't exist exceptions.
+
+    Args:
+    filename
+
+    Return:
+    number of lines in a file
     """
+    line_count = 0
+    with open(filename, "r", encoding='utf-8') as my_file:
+        for line in my_file:
+            line_count += 1
+        return(line_count)
+
