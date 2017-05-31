31eed5cd441adabfd4a0674496a145fbf7169661
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-31 03:45:41 -0700

    Add Task 0 - handle new line error

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -14,5 +14,5 @@ def read_file(filename=""):
     filename
     """
     with open(filename, "r", encoding='utf-8') as my_file:
-        print(my_file.read())
+        print(my_file.read(), end="")
         
