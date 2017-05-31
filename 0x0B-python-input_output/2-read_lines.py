91a43ad852276a5e5f3e216e5e695d1f55a9cf9b
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-31 04:54:05 -0700

    Add Task 2 - v1

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,9 +1,33 @@
 #!/usr/bin/python3
-def read_lines(filename="", nb_lines=0):
-    """Write a function that reads n lines of a text file (UTF8) and prints it to stdout:
+"""
+This is module 2-read_lines
 
-Read the entire file if nb_lines is lower or equal to 0 OR greater or equal to the total number of lines of the file
-You must use the with statement
-You don't need to manage file permission/file doesn't exist exceptions.
+This module contains one function `read_lines`
+"""
+
+
+def read_lines(filename="", nb_lines=0):
     """
-    
+    Write a function that reads n lines of a text file (UTF8) and prints it to stdout:
+    Read the entire file if
+    nb_lines is lower or equal to 0 OR
+    greater or equal to the total number of lines of the file
+    You must use the with statement
+    You don't need to manage file permission/file doesn't exist exceptions.
+
+    Args:
+    filename
+    nb_lines
+
+    Return:
+    prints specific lines to stdout
+    """
+    line_count = 0
+    with open(filename, "r", encoding='utf-8') as my_file:
+        if nb_lines <= 0:
+            print(my_file.read(), end="")
+        else:
+            for line in my_file:
+                if line_count < nb_lines:
+                    print(line, end="")
+                    line_count += 1
