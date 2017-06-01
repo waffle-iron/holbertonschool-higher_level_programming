6b6aafaf311e1177446284b0cbe5874683480df9
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-31 18:29:11 -0700

    Add Task 9 - work in progress

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""
+This is module 9-add_item
+
+This module contains one function `add_item`
+"""
+
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+
+
+def add_item(filename, args):
+    """
+    Write a script that adds all arguments to a Python list, and then saves them to a file
+    *must use your function save_to_json_file from 7-save_to_json_file.py
+    *must use your function load_from_json_file from 8-load_from_json_file.py
+    The list must be saved as a JSON representation in a file named add_item.json
+    If the file doesn't exist, it should be created
+    You don't need to manage file permissions / exceptions.
+
+    Args:
+    filename
+    args
+    """
+    try:
+        my_obj = load_from_json_file(filename)
+    except FileNotFoundError:
+        my_obj = []
+    save_to_json_file(my_obj + args, filename)
+
+if __name__ == "__main__":
+    filename = "add_item.json"
+    load_and_save(filename, sys.argv[1:])
