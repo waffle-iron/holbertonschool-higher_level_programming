08e4ad5d292edd5b36953f232d19503667009d96
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-06-03 03:09:45 -0700

    Update Task 10 docstring

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
-def class_to_json(obj):
-    """Write a function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object:
+"""
+This is module 10-class_to_json
+            
+This module contains one function `class_to_json()`
+"""
 
-obj is an instance of a Class
-All attributes of the obj Class are serializable: list, dictionary, string, integer and boolean
-You are not allowed to import any module
+def class_to_json(obj):
     """
+    function that returns dictionary description with
+    simple data structure (list, dictionary, string,
+    integer and boolean) for JSON serialization of an object:
+            obj is an instance of a Class
+            All attributes of the obj Class are serializable: 
+                list
+                dictionary
+                string
+                integer
+                boolean
+
+    You are not allowed to import any module
+
+    Args:
+    obj
+
+    Returns:
+    dictionary description
+    """
+    
