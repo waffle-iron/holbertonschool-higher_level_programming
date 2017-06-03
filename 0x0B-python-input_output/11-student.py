2b041c19a9de5c78a40532729c78a5c1440e8d0d
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-06-03 04:07:19 -0700

    Add Task 11 - work in progress

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python3
-class Student:
+"""
+This is module 11-student
+
+This module contains one class:
+     `Student`
+
+This module contains one function:
+     `to_json`
+"""
+
+
+class Student():
     """
     Public instance attributes:
         first_name
         last_name
         age
+
     Instantiation with first_name, last_name and age:
     `def __init__(self, first_name, last_name, age)`
 
-    Public method def to_json(self):
-    that retrieves a dictionary representation of a Student instance (same as 10-class_to_json.py)
+    Public class method
+    def to_json(self): retrieves a dictionary representation
+    of a Student instance (same as 10-class_to_json.py)
     """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return (obj.__dict__)
