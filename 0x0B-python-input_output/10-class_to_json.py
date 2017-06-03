a23af2cc63558615b757283d07d0478f8f4986d9
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-06-03 04:29:30 -0700

    Update PEP8 formatting

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""
-This is module 10-class_to_json
-
+"""This is module 10-class_to_json
 This module contains one function `class_to_json()`
 """
+
+
 def class_to_json(obj):
     return (obj.__dict__)
