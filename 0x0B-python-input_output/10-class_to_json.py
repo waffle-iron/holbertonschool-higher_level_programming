2354c7ebcdfbc81d52494667accca8d816cc858e
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-06-03 03:33:01 -0700

    Task 10 - update pep8 formatting

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
 This is module 10-class_to_json
-            
+
 This module contains one function `class_to_json()`
 """
+
 
 def class_to_json(obj):
     """
@@ -11,7 +12,7 @@ def class_to_json(obj):
     simple data structure (list, dictionary, string,
     integer and boolean) for JSON serialization of an object:
             obj is an instance of a Class
-            All attributes of the obj Class are serializable: 
+            All attributes of the obj Class are serializable:
                 list
                 dictionary
                 string
